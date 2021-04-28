2e3d61128b54791e6622a5f735a296f668e308ce
BLOCKMATERIAL <miroslavgrischenko@gmail.com>
2021-04-28 03:02:45 +0300

    Delete import Touchable

--- components/Task.js
+++ components/Task.js
@@ -1,11 +1,5 @@
 import React from "react";
-import {
-  StyleSheet,
-  Text,
-  Touchable,
-  TouchableOpacity,
-  View,
-} from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 const Task = props => {
   return (
